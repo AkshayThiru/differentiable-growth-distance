ac2b28c55bc4ac1c33e19791ed27e7daa2a51c1a
AkshayThiru <akshay_t@berkeley.edu>
2025-02-28 13:29:46 -0800

    Update and document data types and ConvexSet class

--- include/dgd/data_types.h
+++ include/dgd/data_types.h
@@ -1,28 +1,116 @@
+// Copyright 2025 Akshay Thirugnanam
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file data_types.h
+ * @author Akshay Thirugnanam (akshay_t@berkeley.edu)
+ * @date 2025-02-18
+ * @brief Interface for data types and constants.
+ */
+
 #ifndef DGD_DATA_TYPES_H_
 #define DGD_DATA_TYPES_H_
 
 #include <Eigen/Core>
+#include <cmath>
 #include <limits>
 
 namespace dgd {
 
-typedef float Real;
+typedef double Real; /**< Precision of floating-point real numbers. */
 
+/**
+ * @brief Infinity.
+ *
+ * @note This constant is only used for the initial lower bound for the growth
+ * distance algorithm. For a pair of convex sets with circumradii \f$R_1\f$ and
+ * \f$R_2\f$, kInf can be any number greater than \f$R_1 + R_2\f$.
+ */
+constexpr Real kInf{std::numeric_limits<Real>::infinity()};
+constexpr Real kEps{
+    std::numeric_limits<Real>::epsilon()}; /**< Machine epsilon. */
+constexpr Real kEpsSqrt{
+    std::sqrt(kEps)}; /**< Square root of machine epsilon. */
+constexpr Real kPi{static_cast<Real>(EIGEN_PI)}; /**< \f$\pi\f$. */
+
+/**
+ * @brief Fixed dimension vector.
+ *
+ * @tparam dim Dimension of the vector.
+ */
 template <int dim>
 using Vecf = Eigen::Vector<Real, dim>;
 typedef Vecf<2> Vec2f;
 typedef Vecf<3> Vec3f;
 
+/**
+ * @brief Alias for Eigen::MatrixBase.
+ *
+ * @tparam Derived Derived class.
+ */
 template <typename Derived>
 using MatrixBase = Eigen::MatrixBase<Derived>;
 
+/**
+ * @brief Fixed-row, variable-column matrix.
+ *
+ * @note Vectors should be stored along columns, since Eigen stores matrices in
+ * a column-major format (by default).
+ *
+ * @tparam dim Number of rows of the matrix.
+ */
 template <int dim>
 using MatXf = Eigen::Matrix<Real, dim, -1>;
 typedef MatXf<2> Mat2Xf;
 typedef MatXf<3> Mat3Xf;
 
-constexpr Real kInf{std::numeric_limits<Real>::infinity()};
-constexpr Real kEps{std::numeric_limits<Real>::epsilon()};
+/**
+ * @brief Fixed size matrix.
+ *
+ * @note Vectors are stored along columns.
+ *
+ * @tparam row Number of rows of the matrix.
+ * @tparam col Number of columns of the matrix.
+ */
+template <int row, int col>
+using Matf = Eigen::Matrix<Real, row, col>;
+
+/**
+ * @brief Rigid body transformation matrix.
+ *
+ * Rigid body transformations are of the form:
+ * \f[
+ * \left( \matrix{ R & p \cr 0 & 1 \cr} \right),
+ * \f]
+ * where \f$R\f$ is the rotation matrix and \f$p\f$ is the position vector.
+ *
+ * @tparam dim Dimension of the space.
+ */
+template <int dim>
+using Transformf = Matf<dim + 1, dim + 1>;
+typedef Transformf<2> Transform2f;
+typedef Transformf<3> Transform3f;
+
+/**
+ * @brief Rotation matrix.
+ *
+ * @tparam dim Dimension of the space.
+ */
+template <int dim>
+using Rotf = Matf<dim, dim>;
+typedef Rotf<2> Rot2f;
+typedef Rotf<3> Rot3f;
 
 }  // namespace dgd
 
--- include/dgd/geometry/convex_set.h
+++ include/dgd/geometry/convex_set.h
@@ -1,3 +1,24 @@
+// Copyright 2025 Akshay Thirugnanam
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file convex_set.h
+ * @author Akshay Thirugnanam (akshay_t@berkeley.edu)
+ * @date 2025-02-18
+ * @brief Convex set abstract class implementation.
+ */
+
 #ifndef DGD_GEOMETRY_CONVEX_SET_H_
 #define DGD_GEOMETRY_CONVEX_SET_H_
 
@@ -10,73 +31,106 @@ namespace dgd {
 /**
  * @brief Convex set abstract class implementing the support function.
  *
- * @tparam dim
+ * @attention The convex set must be a compact and solid set (i.e., closed,
+ * bounded, and with a nonempty interior). Also, the origin must be in
+ * the interior of the set.
  *
- * @note The convex set must be compact and solid. Also, 0 \in \mathbb{R}^dim
- * must be in the interior of the convex set (TODO: remove this requirement).
+ * @tparam dim Dimension of the convex set (2 or 3).
  */
 template <int dim>
 class ConvexSet {
  protected:
+  /**
+   * @brief Initializes convex set properties.
+   *
+   * @see ConvexSet(Real inradius)
+   */
   ConvexSet();
 
-  ConvexSet(Real margin);
+  /**
+   * @brief Initializes convex set inradius.
+   *
+   * @param inradius Inradius for the convex set.
+   * @see inradius_
+   */
+  ConvexSet(Real inradius);
 
   /**
-   * @brief Construct a new Convex Set object
+   * @brief Inradius for the convex set.
    *
-   * @param margin
-   * @param inradius (any number greater than zero and less than the radius of
-   * the maximal circle at 0.)
+   * Radius of a ball that is centered at the origin and contained in the set.
+   * Any number greater than 0 and less than the Chebyshev radius will work.
+   * However, larger values can help prevent singularities in simplex
+   * computations.
    */
-  ConvexSet(Real margin, Real inradius);
-
-  Real margin_;
   Real inradius_;
 
  public:
+  /**
+   * @brief Destroys the Convex Set object.
+   */
   virtual ~ConvexSet() {}
 
   /**
-   * @brief Support function for the convex set.
+   * @brief Implements the support function.
    *
-   * @param n (normalized)
-   * @param sp
-   * @return v
+   * Implements the support function for the convex set \f$C\f$, which is given
+   * by: \f{align*}{
+   * sv & = \max_{x \in C} \langle n, x\rangle, \\
+   * sp & \in \arg \max_{x \in C} \langle n, x\rangle,
+   * \f}
+   * where \f$sv\f$ is the return value of the function.
+   *
+   * @note Safety margins (in the 2-norm) can be directly included in the
+   * support function computation as: \f{align*}{
+   * sv & \leftarrow sv + m, \\
+   * sp & \leftarrow sp + m \cdot n,
+   * \f}
+   * where \f$m\f$ is the safety margin. Note that the safety margin also
+   * increases the inradius.
+   *
+   * @param[in]  n  Normal vector with unit 2-norm.
+   * @param[out] sp Support point. A point at which the maximum for the
+   * support function is attained.
+   * @return     Value of the support function at the normal vector.
    */
   virtual Real SupportFunction(const Vecf<dim>& n, Vecf<dim>& sp) const = 0;
 
-  Real GetMargin() const;
+  /**
+   * @brief Gets the dimension of the convex set.
+   *
+   * @return Dimension, given by the template parameter dim.
+   */
+  static constexpr int Dimension();
 
-  void SetMargin(Real margin);
-
+  /**
+   * @brief Gets the inradius.
+   *
+   * @return Inradius.
+   * @see inradius_
+   */
   Real GetInradius() const;
 
+  /**
+   * @brief Sets the inradius.
+   *
+   * @param[in] inradius Inradius (\f$> 0\f$).
+   * @see inradius_
+   */
   void SetInradius(Real inradius);
 };
 
 template <int dim>
-inline ConvexSet<dim>::ConvexSet() : ConvexSet(Real(0.0), kEps) {}
+inline ConvexSet<dim>::ConvexSet() : ConvexSet(kEps) {}
 
 template <int dim>
-inline ConvexSet<dim>::ConvexSet(Real margin) : ConvexSet(margin, kEps) {}
-
-template <int dim>
-inline ConvexSet<dim>::ConvexSet(Real margin, Real inradius)
-    : margin_(margin), inradius_(inradius) {
-  assert(margin >= Real(0.0));
+inline ConvexSet<dim>::ConvexSet(Real inradius) : inradius_(inradius) {
   assert(inradius > Real(0.0));
 }
 
 template <int dim>
-inline Real ConvexSet<dim>::GetMargin() const {
-  return margin_;
-}
-
-template <int dim>
-inline void ConvexSet<dim>::SetMargin(Real margin) {
-  assert(margin >= Real(0.0));
-  margin_ = margin;
+constexpr int ConvexSet<dim>::Dimension() {
+  return dim;
 }
 
 template <int dim>
